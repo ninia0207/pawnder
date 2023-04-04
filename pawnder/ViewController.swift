307d223835860e15870c2dd03898b683c16a5fed
ninia0207 <86304060+ninia0207@users.noreply.github.com>
2023-04-04 08:54:26 +0400

    Initial Commit

---
+++ pawnder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pawnder
+//
+//  Created by Ninia Sabadze on 04.04.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
