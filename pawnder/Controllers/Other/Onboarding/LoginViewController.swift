8e0a8c741bef6e3e6b8ecd8a760638526691ea2c
ninia0207 <86304060+ninia0207@users.noreply.github.com>
2023-04-07 01:07:06 +0400

    Third commit

--- pawnder/Controllers/Other/Onboarding/LoginViewController.swift
+++ pawnder/Controllers/Other/Onboarding/LoginViewController.swift
@@ -9,39 +9,106 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    struct Constants{
+        static let cornerRadius : CGFloat = 8.0
+    }
+    
+    //unanimous closure
     private let usernameEmailField : UITextField = {
-        return UITextField()
+        let field = UITextField()
+        field.placeholder = "Username or Email"
+        field.returnKeyType = .next
+        field.leftViewMode = .always
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.layer.masksToBounds = true
+        field.layer.cornerRadius = Constants.cornerRadius
+        field.backgroundColor = .secondarySystemBackground
+        field.layer.borderWidth = 1.0
+        field.layer.borderColor = UIColor.secondaryLabel.cgColor
+        return field
     }()
     
     private let passwordField : UITextField = {
         let field = UITextField()
         field.isSecureTextEntry = true
+        field.placeholder = "Password"
+        field.returnKeyType = .continue
+        field.leftViewMode = .always
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.layer.masksToBounds = true
+        field.layer.cornerRadius = Constants.cornerRadius
+        field.backgroundColor = .secondarySystemBackground
+        field.layer.borderWidth = 1.0
+        field.layer.borderColor = UIColor.secondaryLabel.cgColor
         return field
     }()
     
+    //buttons
     private let loginButton : UIButton = {
-        return UIButton()
+        let button = UIButton()
+        button.setTitle("Log In", for: .normal)
+        button.layer.masksToBounds = true
+        button.layer.cornerRadius = Constants.cornerRadius
+        button.backgroundColor = .systemGray
+        button.setTitleColor(.white, for: .normal)
+        return button
     }()
     
-    private let headerView : UIView = {
-        return UIView()
+    private let createAccountButton : UIButton = {
+        let button = UIButton()
+        button.setTitleColor(.label, for: .normal)
+        button.setTitle("Create an Account", for: .normal)
+        
+        return button
     }()
     
     private let termsButton : UIButton = {
+        let button = UIButton()
+        button.setTitle("Terms of Service", for: .normal)
+        button.setTitleColor(.secondaryLabel, for: .normal)
         return UIButton()
     }()
     
     private let privacyButton : UIButton = {
+        let button = UIButton()
+        button.setTitle("Privacy", for: .normal)
+        button.setTitleColor(.secondaryLabel, for: .normal)
         return UIButton()
     }()
     
-    private let createAccountButton : UIButton = {
-        return UIButton()
+    
+    //header
+    private let headerView : UIView = {
+        let header = UIView()
+        header.layer.masksToBounds = true
+        let backgroundView = UIImageView(image: UIImage(named: "Gradient"))
+        header.addSubview(backgroundView)
+        return header
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        loginButton.addTarget(self,
+                              action: #selector(didTapLoginButton),
+                              for: .touchUpInside)
+        createAccountButton.addTarget(self,
+                              action: #selector(didTapCreateAccountButton),
+                              for: .touchUpInside)
+        termsButton.addTarget(self,
+                              action: #selector(didTapTermButton),
+                              for: .touchUpInside)
+        privacyButton.addTarget(self,
+                              action: #selector(didTapPrivacyButton),
+                              for: .touchUpInside)
+        
+        usernameEmailField.delegate = self
+        passwordField.delegate = self
+        addSubviews()
         view.backgroundColor = .systemBackground
     }
     
@@ -49,6 +116,74 @@ class LoginViewController: UIViewController {
         super.viewDidLayoutSubviews()
         
         //assign frames
+        
+        headerView.frame = CGRect(
+            x: 0,
+            y: 0.0,
+            width: view.width,
+            height: view.height/3.0
+        )
+        
+        usernameEmailField.frame = CGRect(
+            x: 25,
+            y: headerView.bottom + 40,
+            width: view.width-50,
+            height: 52.0
+        )
+        
+        passwordField.frame = CGRect(
+            x: 25,
+            y: usernameEmailField.bottom + 10,
+            width: view.width-50,
+            height: 52.0
+        )
+        
+        loginButton.frame = CGRect(
+            x: 25,
+            y: passwordField.bottom + 10,
+            width: view.width-50,
+            height: 52.0
+        )
+        
+        createAccountButton.frame = CGRect(
+            x: 25,
+            y: loginButton.bottom + 10,
+            width: view.width-50,
+            height: 52.0
+        )
+        
+        termsButton.frame = CGRect(
+            x: 10,
+            y: view.height-view.safeAreaInsets.bottom-100,
+            width: view.width-20,
+            height: 50
+        )
+        
+        privacyButton.frame = CGRect(
+            x: 10,
+            y: view.height-view.safeAreaInsets.bottom-50,
+            width: view.width-20,
+            height: 50
+        )
+        configureHeaderView()
+    }
+    
+    private func configureHeaderView(){
+        guard headerView.subviews.count == 1 else{
+            return
+        }
+        
+        guard let backgroundView = headerView.subviews.first else{
+            return
+        }
+        backgroundView.frame = headerView.bounds
+        
+        //add logo
+        let imageView = UIImageView(image: UIImage(named: "PawnderLogo"))
+        headerView.addSubview(imageView)
+        imageView.contentMode = .scaleAspectFit
+        imageView.frame = CGRect(x: headerView.width/4.0, y: view.safeAreaInsets.top, width: headerView.width/2.0, height: headerView.height - view.safeAreaInsets.top)
+        
     }
     
     private func addSubviews(){
@@ -62,6 +197,15 @@ class LoginViewController: UIViewController {
     }
     
     @objc private func didTapLoginButton(){
+        passwordField.resignFirstResponder()
+        usernameEmailField.resignFirstResponder()
+        
+        guard let usernameEmail = usernameEmailField.text, !usernameEmail.isEmpty,
+              let password = passwordField.text, !password.isEmpty, password.count >= 8 else{
+            return
+        }
+        
+        //login functionality
         
     }
     @objc private func didTapTermButton(){
@@ -71,6 +215,20 @@ class LoginViewController: UIViewController {
         
     }
     @objc private func didTapCreateAccountButton(){
+        let vc = RegistrationViewController()
+        present(vc, animated: true)
+    }
+}
+
+extension LoginViewController : UITextFieldDelegate {
+    func textFieldShouldButton(textField: UITextField) -> Bool{
+        if textField == usernameEmailField{
+            passwordField.becomeFirstResponder()
+        }
+        else if textField == passwordField{
+            didTapLoginButton()
+        }
         
+        return true
     }
 }
---
+++ pawnder/Resources/Extensions.swift
@@ -0,0 +1,36 @@
+//
+//  Extensions.swift
+//  pawnder
+//
+//  Created by Ninia Sabadze on 07.04.23.
+//
+
+import UIKit
+
+extension UIView{
+    
+    public var width : CGFloat {
+        return frame.width
+    }
+    
+    public var height : CGFloat {
+        return frame.height
+    }
+    
+    public var top : CGFloat {
+        return frame.origin.y
+    }
+    
+    public var bottom : CGFloat {
+        return frame.origin.y + frame.size.height
+    }
+    
+    public var left : CGFloat {
+        return frame.origin.x
+    }
+    
+    public var right : CGFloat {
+        return frame.origin.x + frame.size.width
+    }
+    
+}
